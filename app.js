8d9d5dd90ffb5e35b092a5f83c7576062022fc4e
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-13 07:30:28 +0200

    push exercice après correction et commentaires

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-function changePicture(){
-  document.getElementById('img1').src="./image1_2.jpg";
+// dans cette fonction on cible par la méthode getElementById du DOM
+// un élément dont l'id correspond à la chaîne de caractères spécifiée,
+// on peut modifier l'attribut src de l'image (src="./image1_2.jpg") avec l'ID "img1"
+function changePicture(toto){
+  document.getElementById(toto).src="./image1_2.jpg";
 }
